731758d1440258cbf2f26f83b57af44290780370
Oskar Agzigitov <110802094+AgzigitovOskar@users.noreply.github.com>
2023-04-19 12:51:57 +0300

    Lesson_35

--- todolist/core/views.py
+++ todolist/core/views.py
@@ -1,7 +1,47 @@
-from rest_framework import generics
+from typing import Any
 
-from todolist.core.serializers import CreateUserSerializer
+from django.contrib.auth import login, logout
+from django.http import JsonResponse
+from rest_framework import generics, status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.request import Request
+from rest_framework.response import Response
+
+from .serializers import CreateUserSerializer, LoginSerializer, ProfileSerializer, UpdatePasswordSerializer
 
 
 class SignUpView(generics.CreateAPIView):
     serializer_class = CreateUserSerializer
+
+
+class LoginView(generics.CreateAPIView):
+    serializer_class = LoginSerializer
+
+    def create(self, request: Request, *args: Any, **kwargs: Any) -> Response:
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        login(request=request, user=serializer.save())
+        return Response(serializer.data)
+
+
+class ProfileView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ProfileSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
+
+    def delete(self, request: Request, *args, **kwargs) -> JsonResponse:
+        logout(request)
+        return JsonResponse({'message': 'Logout successfully complete'}, safe=False, status=status.HTTP_204_NO_CONTENT)
+
+    # def perform_destroy(self, instance):
+    #     logout(self.request)
+
+
+class UpdatePasswordView(generics.UpdateAPIView):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = UpdatePasswordSerializer
+
+    def get_object(self):
+        return self.request.user
--- todolist/urls.py
+++ todolist/urls.py
@@ -19,10 +19,11 @@ from django.urls import path, include
 
 urlpatterns = [
     path('core/', include(('todolist.core.urls', 'todolist.core'))),
-    path('admin/', admin.site.urls)
+    path('goals/', include(('todolist.goals.urls', 'todolist.goals'))),
+    path('admin/', admin.site.urls),
+    path('oauth/', include('social_django.urls', namespace='social')),
 ]
 
+
 if settings.DEBUG:
-    urlpatterns = [
-        path('api-auth/', include('rest_framework.urls'))
-    ]
+    urlpatterns += [path('api-auth/', include('rest_framework.urls'))]
