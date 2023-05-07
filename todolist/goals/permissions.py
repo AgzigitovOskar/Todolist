9557e559b9eab50593d50253019dc5cb355e768a
Oskar Agzigitov <110802094+AgzigitovOskar@users.noreply.github.com>
2023-05-08 00:33:18 +0300

    TODOLIST->37->finish

--- todolist/goals/permissions.py
+++ todolist/goals/permissions.py
@@ -76,3 +76,4 @@ class GoalPermissions(IsAuthenticated):
 #             _filters['role__in'] = [BoardParticipant.Role.owner, BoardParticipant.Role.writer]
 #
 #         return BoardParticipant.objects.filter(**_filters).exists()
+
