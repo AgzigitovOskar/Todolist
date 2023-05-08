b80679e67b8b155321f56329933df6dc4fc11e08
Oskar Agzigitov <110802094+AgzigitovOskar@users.noreply.github.com>
2023-05-08 11:08:45 +0300

    TODOLIST->37->fix

--- todolist/goals/permissions.py
+++ todolist/goals/permissions.py
@@ -43,37 +43,3 @@ class GoalPermissions(IsAuthenticated):
             _filters['role__in'] = [BoardParticipant.Role.owner, BoardParticipant.Role.writer]
 
         return BoardParticipant.objects.filter(**_filters).exists()
-
-# class BoardPermissions(IsAuthenticated):
-#     def has_object_permission(self, request: Request, view, obj: Board) -> bool:
-#         _filters: dict[str:Any] = {'user_id': request.user.id, 'board_id': obj.id}
-#         if request.metod not in SAFE_METHODS:
-#             _filters['role'] = BoardParticipant.Role.owner
-#
-#         return BoardParticipant.objects.filter(**_filters).exists()
-#
-#
-# class GoalCategoryPermissions(IsAuthenticated):
-#     def has_object_permission(self, request: Request, view, goal_gategory: GoalCategory) -> bool:
-#         _filters: dict[str:Any] = {'user_id': request.user.id, 'board_id': goal_gategory.board_id}
-#         if request.metod not in SAFE_METHODS:
-#             _filters['role'] = BoardParticipant.Role.owner
-#
-#         return BoardParticipant.objects.filter(**_filters).exists()
-#
-#
-# class IsOwnerOrReadOnly(BasePermission):
-#     def has_object_permission(self, request, view, obj):
-#         if request.method in SAFE_METHODS:
-#             return True
-#         return obj.user.id == request.user.id
-#
-#
-# class GoalPermissions(IsAuthenticated):
-#     def has_object_permission(self, request, view, obj):
-#         _filters: dict = {'user': request.user, 'board': obj.category.board}
-#         if request.method not in SAFE_METHODS:
-#             _filters['role__in'] = [BoardParticipant.Role.owner, BoardParticipant.Role.writer]
-#
-#         return BoardParticipant.objects.filter(**_filters).exists()
-
